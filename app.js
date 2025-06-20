df5e5f5b547fcd52f33b9d08e1fe64d19fb1d945
Ktab.izhan <ktab.izhan@jsbl.com>
2025-06-20 15:54:22 +0500

    local

--- app.js
+++ app.js
@@ -10,26 +10,26 @@ require('dotenv').config();
 
 
 const allowedOrigins = [
-  'http://localhost:3000', // Development
- 
-  null, // Allow null origins (e.g., Postman, local files)
+    'http://localhost:3000', // Development
+    'https://sbt-fe.vercel.app/',
+    null, 
 ];
 
 app.use(
-  cors({
-    origin: function (origin, callback) {
-      console.log('🌍 Request Origin:', origin);
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        console.log(`❌ Blocked by CORS: ${origin}`);
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  })
+    cors({
+        origin: function (origin, callback) {
+            console.log('🌍 Request Origin:', origin);
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                console.log(`❌ Blocked by CORS: ${origin}`);
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    })
 );
 
 // Middleware
@@ -38,17 +38,17 @@ app.use(morgan('dev'));
 
 // Database connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('Database Connected'))
-  .catch(err => console.error('Error connecting to database:', err));
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('Database Connected'))
+    .catch(err => console.error('Error connecting to database:', err));
 
 // Routes
 app.use('/api', Routes);
 
 // Start server
 app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
+    console.log(`Server is listening at http://localhost:${port}`);
 });
