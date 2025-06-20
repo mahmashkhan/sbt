c7dc0008c5138a5fd69f6189a3d3074e455bf3fa
Ktab.izhan <ktab.izhan@jsbl.com>
2025-06-20 12:05:56 +0500

    env

--- app.js
+++ app.js
@@ -4,17 +4,19 @@ const port = 4000
 const Routes = require('./src/index')
 const cors = require('cors');
 const mongoose = require('mongoose');
+require("dotenv").config();
+const morgan = require("morgan");
 
 // Middleware to parse JSON
 app.use(express.json());
-
+app.use(morgan("dev"));
 app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type']
-}))
+  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"],
+}));
 
-mongoose.connect('mongodb+srv://mahmashak08:Legend786sbt@cluster0.6oflxia.mongodb.net/TAHA_PORTFOLIO?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
