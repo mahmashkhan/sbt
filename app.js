72a8aff0bf43fc761da0523611b4f4ac3730eeee
Ktab.izhan <ktab.izhan@jsbl.com>
2025-06-20 16:00:26 +0500

    local

--- app.js
+++ app.js
@@ -12,25 +12,11 @@ require('dotenv').config();
 const allowedOrigins = [
     'http://localhost:3000', // Development
     'https://sbt-fe.vercel.app/',
-    null, 
+    null,
 ];
 
 app.use(
-    cors({
-        origin: function (origin, callback) {
-            console.log('🌍 Request Origin:', origin);
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                console.log(`❌ Blocked by CORS: ${origin}`);
-                callback(new Error('Not allowed by CORS'));
-            }
-        },
-        credentials: true,
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
-    })
-);
+    cors());
 
 // Middleware
 app.use(express.json());
